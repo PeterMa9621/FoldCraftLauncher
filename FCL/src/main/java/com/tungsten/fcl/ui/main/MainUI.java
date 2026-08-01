4aa07c67832c387c2b182dd0ac0bd7d44b561cae
mxc <gg1996@163.com>
2026-08-02 01:27:37 +0800

    请去掉 App 进入主页后的 "公告： 关于x.x.x."

--- FCL/src/main/java/com/tungsten/fcl/ui/main/MainUI.java
+++ FCL/src/main/java/com/tungsten/fcl/ui/main/MainUI.java
@@ -4,43 +4,21 @@ import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.view.View;
 
-import androidx.appcompat.widget.LinearLayoutCompat;
-
 import com.tungsten.fcl.R;
 import com.tungsten.fcl.game.TexturesLoader;
 import com.tungsten.fcl.setting.Accounts;
-import com.tungsten.fcl.util.AndroidUtils;
 import com.tungsten.fclcore.auth.Account;
 import com.tungsten.fclcore.fakefx.beans.property.ObjectProperty;
 import com.tungsten.fclcore.fakefx.beans.property.SimpleObjectProperty;
 import com.tungsten.fclcore.task.Schedulers;
 import com.tungsten.fclcore.task.Task;
-import com.tungsten.fclcore.util.Logging;
-import com.tungsten.fclcore.util.io.HttpRequest;
-import com.tungsten.fcllibrary.component.dialog.FCLAlertDialog;
 import com.tungsten.fcllibrary.component.theme.ThemeEngine;
 import com.tungsten.fcllibrary.component.ui.FCLCommonUI;
-import com.tungsten.fcllibrary.component.view.FCLButton;
-import com.tungsten.fcllibrary.component.view.FCLTextView;
 import com.tungsten.fcllibrary.component.view.FCLUILayout;
 import com.tungsten.fcllibrary.skin.SkinRenderer;
 import com.tungsten.fcllibrary.skin.SkinViewer;
-import com.tungsten.fcllibrary.util.LocaleUtils;
 
-import java.util.logging.Level;
-
-public class MainUI extends FCLCommonUI implements View.OnClickListener {
-
-    public static final String ANNOUNCEMENT_URL = "https://raw.githubusercontent.com/FCL-Team/FCL-Repo/refs/heads/main/res/announcement_v2.txt";
-    public static final String ANNOUNCEMENT_URL_CN = "https://gitee.com/fcl-team/FCL-Repo/raw/main/res/announcement_v2.txt";
-
-    private LinearLayoutCompat announcementContainer;
-    private LinearLayoutCompat announcementLayout;
-    private FCLTextView title;
-    private FCLTextView announcementView;
-    private FCLTextView date;
-    private FCLButton hide;
-    private Announcement announcement = null;
+public class MainUI extends FCLCommonUI {
 
     private SkinViewer skinViewer;
     private SkinRenderer renderer;
@@ -54,19 +32,9 @@ public class MainUI extends FCLCommonUI implements View.OnClickListener {
     @Override
     public void onCreate() {
         super.onCreate();
-        announcementContainer = findViewById(R.id.announcement_container);
-        announcementLayout = findViewById(R.id.announcement_layout);
-        title = findViewById(R.id.title);
-        announcementView = findViewById(R.id.announcement);
-        date = findViewById(R.id.date);
-        hide = findViewById(R.id.hide);
-        ThemeEngine.getInstance().registerEvent(announcementLayout, () -> announcementLayout.getBackground().setTint(ThemeEngine.getInstance().getTheme().getColor()));
-        hide.setOnClickListener(this);
-
         skinViewer = findViewById(R.id.skin_viewer);
         renderer = new SkinRenderer(getContext());
         skinViewer.setRenderer(renderer, 5f);
-        checkAnnouncement();
         setupSkinDisplay();
     }
 
@@ -119,31 +87,6 @@ public class MainUI extends FCLCommonUI implements View.OnClickListener {
         });
     }
 
-    private void checkAnnouncement() {
-        try {
-            String url = LocaleUtils.isChinese(getContext()) ? ANNOUNCEMENT_URL_CN : ANNOUNCEMENT_URL;
-            Task.supplyAsync(() -> HttpRequest.HttpGetRequest.GET(url).getJson(Announcement.class))
-                    .thenAcceptAsync(Schedulers.androidUIThread(), announcement -> {
-                        this.announcement = announcement;
-                        if (!announcement.shouldDisplay(getContext()))
-                            return;
-                        announcementContainer.setVisibility(View.VISIBLE);
-                        title.setText(AndroidUtils.getLocalizedText(getContext(), "announcement", announcement.getDisplayTitle(getContext())));
-                        announcementView.setText(announcement.getDisplayContent(getContext()));
-                        date.setText(AndroidUtils.getLocalizedText(getContext(), "update_date", announcement.getDate()));
-                    }).start();
-        } catch (Exception e) {
-            Logging.LOG.log(Level.WARNING, "Failed to get announcement!", e);
-        }
-    }
-
-    private void hideAnnouncement() {
-        announcementContainer.setVisibility(View.GONE);
-        if (announcement != null) {
-            announcement.hide(getContext());
-        }
-    }
-
     private void setupSkinDisplay() {
         currentAccount = new SimpleObjectProperty<>() {
 
@@ -170,20 +113,4 @@ public class MainUI extends FCLCommonUI implements View.OnClickListener {
         });
     }
 
-    @Override
-    public void onClick(View view) {
-        if (view == hide) {
-            if (announcement != null && announcement.isSignificant()) {
-                FCLAlertDialog.Builder builder = new FCLAlertDialog.Builder(getContext());
-                builder.setAlertLevel(FCLAlertDialog.AlertLevel.ALERT);
-                builder.setCancelable(false);
-                builder.setMessage(getContext().getString(R.string.announcement_significant));
-                builder.setPositiveButton(this::hideAnnouncement);
-                builder.setNegativeButton(null);
-                builder.create().show();
-            } else {
-                hideAnnouncement();
-            }
-        }
-    }
 }
